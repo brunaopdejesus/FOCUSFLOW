1c456616188fa8bfa005211253d79958f8b9325a
Bruna Oliveira <bruna.opdejesus@gmail.com>
2024-10-25 13:56:47 -0300

    dados de smartwatch e download de csv

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import joblib
 import os
+import time
 
 app = Flask(__name__)
 
@@ -23,15 +24,6 @@ smartwatch_dados = {
 
 # Caminho para o arquivo CSV do smartwatch
 csv_smartwatch_path = './dados_smartwatch.csv'
-
-# Função para salvar dados do smartwatch no CSV
-def salvar_no_csv_smartwatch(pulse, status_funcionario, myo):
-    file_exists = os.path.isfile(csv_smartwatch_path)
-    with open(csv_smartwatch_path, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        if not file_exists:
-            writer.writerow(['pulse', 'status_funcionario', 'myo'])
-        writer.writerow([pulse, status_funcionario, myo])
 
 @app.route('/')
 def index():
@@ -56,6 +48,10 @@ def admin():
 @app.route('/circuito')
 def circuito():
     return render_template('circuito.html')
+
+@app.route('/smartwatch')
+def smartwatch():
+    return render_template('smartwatch.html')
 
 @app.route('/termos')
 def termos():
@@ -89,22 +85,35 @@ def get_dados():
 @app.route('/api/smartwatch_dados', methods=['POST'])
 def receber_dados_smartwatch():
     dados = request.json
-    print(f"Dados recebidos no POST: {dados}")  # Log para verificar o conteúdo do POST
     if 'pulse' in dados and 'status_funcionario' in dados and 'myo' in dados:
         smartwatch_dados['pulse'] = dados['pulse']
         smartwatch_dados['status_funcionario'] = dados['status_funcionario']
         smartwatch_dados['myo'] = dados['myo']
-        print(f"Dados armazenados: {smartwatch_dados}")  # Verifique se os dados são armazenados corretamente
+        
+        # Salvar no CSV
+        salvar_no_csv_smartwatch(dados['pulse'], dados['status_funcionario'], dados['myo'])
+        
         return jsonify({'message': 'Dados recebidos com sucesso!'}), 200
     else:
-        print("Dados inválidos recebidos")  # Caso a estrutura dos dados seja errada
         return jsonify({'error': 'Dados inválidos!'}), 400
-
 
 @app.route('/api/smartwatch_get_dados', methods=['GET'])
 def obter_dados_smartwatch():
     # Retorna os dados atuais do smartwatch
     return jsonify(smartwatch_dados), 200
+
+@app.route('/download_smartwatch')
+def download_smartwatch():
+    return send_file(csv_smartwatch_path, as_attachment=True)
+
+# Função para salvar dados do smartwatch no CSV
+def salvar_no_csv_smartwatch(pulse, status_funcionario, myo):
+    file_exists = os.path.isfile(csv_smartwatch_path)
+    with open(csv_smartwatch_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(['timestamp', 'pulse', 'status_funcionario', 'myo'])
+        writer.writerow([time.strftime("%Y-%m-%d %H:%M:%S"), pulse, status_funcionario, myo])
 
 @app.route('/atualizar_tabela')
 def atualizar_tabela():
@@ -144,42 +153,42 @@ def dados():
 
     return jsonify(resultados)
 
-@app.route('/dadosModeloSujestões')
-def dados():
-    # Carregar o modelo treinado e gerar previsões
-    modelo = joblib.load('modeloNotificacao.pkl')
-    data = pd.read_csv('./dataset_estresse_variante.csv')
-    label_encoder = LabelEncoder()
-    label_encoder.fit(data['nivel_estresse'])
+# @app.route('/dadosModeloSujestões')
+# def dados():
+#     # Carregar o modelo treinado e gerar previsões
+#     modelo = joblib.load('modeloNotificacao.pkl')
+#     data = pd.read_csv('./dataset_estresse_variante.csv')
+#     label_encoder = LabelEncoder()
+#     label_encoder.fit(data['nivel_estresse'])
  
-    novo_dado = pd.read_csv('./dadosNotificacao.csv')
-    previsao_novo_dado = modelo.predict(novo_dado)
-    nivel_estresse_previsto = label_encoder.inverse_transform(previsao_novo_dado)[0]
+#     novo_dado = pd.read_csv('./dadosNotificacao.csv')
+#     previsao_novo_dado = modelo.predict(novo_dado)
+#     nivel_estresse_previsto = label_encoder.inverse_transform(previsao_novo_dado)[0]
  
-    # Lógica de sugestões personalizadas
-    sugestoes = []
-    if nivel_estresse_previsto == 'Alto':
-        sugestoes.append("Recomenda-se uma pausa e alongamento.")
-        sugestoes.append("Verificar as condições de trabalho para mais conforto.")
-    elif nivel_estresse_previsto == 'Moderado':
-        sugestoes.append("Realizar algumas respirações profundas.")
-        sugestoes.append("Considere um breve descanso.")
-    else:
-        sugestoes.append("Continue com o ótimo trabalho!")
-        sugestoes.append("Siga o fluxo normal do dia.")
+#     # Lógica de sugestões personalizadas
+#     sugestoes = []
+#     if nivel_estresse_previsto == 'Alto':
+#         sugestoes.append("Recomenda-se uma pausa e alongamento.")
+#         sugestoes.append("Verificar as condições de trabalho para mais conforto.")
+#     elif nivel_estresse_previsto == 'Moderado':
+#         sugestoes.append("Realizar algumas respirações profundas.")
+#         sugestoes.append("Considere um breve descanso.")
+#     else:
+#         sugestoes.append("Continue com o ótimo trabalho!")
+#         sugestoes.append("Siga o fluxo normal do dia.")
  
-    # Criar um dicionário com os resultados
-    resultados = {
-        "previsao_nivel_estresse": nivel_estresse_previsto,
-        "media_batimentos": novo_dado['media_batimentos'].tolist(),
-        "menor_batimento": novo_dado['menor_batimento'].tolist(),
-        "maior_batimento": novo_dado['maior_batimento'].tolist(),
-        "pressao_menor_min": novo_dado['pressao_menor_min'].tolist(),
-        "pressao_maior_min": novo_dado['pressao_maior_min'].tolist(),
-        "sugestoes": sugestoes  # Adicionar as sugestões ao JSON de resposta
-    }
+#     # Criar um dicionário com os resultados
+#     resultados = {
+#         "previsao_nivel_estresse": nivel_estresse_previsto,
+#         "media_batimentos": novo_dado['media_batimentos'].tolist(),
+#         "menor_batimento": novo_dado['menor_batimento'].tolist(),
+#         "maior_batimento": novo_dado['maior_batimento'].tolist(),
+#         "pressao_menor_min": novo_dado['pressao_menor_min'].tolist(),
+#         "pressao_maior_min": novo_dado['pressao_maior_min'].tolist(),
+#         "sugestoes": sugestoes  # Adicionar as sugestões ao JSON de resposta
+#     }
  
-    return jsonify(resultados)
+#     return jsonify(resultados)
 
 
 
